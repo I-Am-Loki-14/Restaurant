820717d62c6937e228533167673eaa7b2784cf10
Min Naing Oo <minnaingoo792@gmail.com>
2024-06-08 17:01:31 +0630

    java-script

---
+++ javascript/restaurant-menu.js
@@ -0,0 +1,10 @@
+let bars = document.querySelector("#bars")
+console.log(bars)
+
+let navLinks = document.querySelector(".nav-links")
+console.log(navLinks)
+
+bars.addEventListener("click", () =>{
+    navLinks.classList.toggle("show")
+    bars.classList.toggle("fa-times")
+})
